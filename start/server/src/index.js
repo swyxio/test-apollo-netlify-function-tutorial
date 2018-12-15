b8b3bfd5d29526383661acdd564fcbb6f58f5ab6
sw-yx <swyx@dontemail.me>
2018-12-16 01:35:13 +1300

    up to step 5

---
+++ start/server/src/index.js
@@ -0,0 +1,36 @@
+const { ApolloServer } = require('apollo-server');
+const typeDefs = require('./schema');
+const resolvers = require('./resolvers');
+const { createStore } = require('./utils');
+
+const LaunchAPI = require('./datasources/launch');
+const UserAPI = require('./datasources/user');
+
+const store = createStore();
+const isEmail = require('isemail');
+
+const server = new ApolloServer({
+  context: async ({ req }) => {
+    // simple auth check on every request
+    const auth = (req.headers && req.headers.authorization) || '';
+    const email = new Buffer(auth, 'base64').toString('ascii');
+
+    // if the email isn't formatted validly, return null for user
+    if (!isEmail.validate(email)) return { user: null };
+    // find a user by their email
+    const users = await store.users.findOrCreate({ where: { email } });
+    const user = users && users[0] ? users[0] : null;
+
+    return { user: { ...user.dataValues } };
+  },
+  typeDefs,
+  resolvers,
+  dataSources: () => ({
+    launchAPI: new LaunchAPI(),
+    userAPI: new UserAPI({ store })
+  })
+});
+
+server.listen().then(({ url }) => {
+  console.log(`🚀 Server ready at ${url}`);
+});
